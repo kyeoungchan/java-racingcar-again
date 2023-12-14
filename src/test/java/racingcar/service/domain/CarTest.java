ae5ae9a1be3cf60e20daaeadb3f08240e3783cfb
kyeoungchan <wkc1002@naver.com>
2023-12-14 13:43:30 +0900

    test: RandomEngine gives movingStatus to Car

--- src/test/java/racingcar/service/domain/CarTest.java
+++ src/test/java/racingcar/service/domain/CarTest.java
@@ -5,41 +5,32 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import racingcar.vo.CarInfo;
 
-import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class CarTest {
 
-    private static final int NOT_MOVE = 1;
-    private static final int MOVE = 4;
-
     @ParameterizedTest
-    @CsvSource(value = {"2, 0", "4, 1", "5, 1"})
-    @DisplayName("랜덤의 값이 4 이상이면 1만큼 더 움직이고, 4 미만이면 움직이지 않는다.")
-    void playOneRound(int randomNumber, int newPosition) {
+    @CsvSource(value = {"false, 0", "true, 1", "true, 1"})
+    @DisplayName("RandomEngine 이 MovingStatus 로 true 를 주면 움직이고, false면 움직이지 않는다.")
+    void playOneRound(boolean movingStatus, int newPosition) {
         int round = 1;
         Car car = new Car("kyeou");
-        assertRandomNumberInRangeTest(() -> {
-            CarInfo carInfo = car.playOneRound(round);
-            assertThat(carInfo.name()).isEqualTo("kyeou");
-            assertThat(carInfo.currentPosition()).isEqualTo(newPosition);
-            assertThat(carInfo.currentRound()).isEqualTo(round);
-        }, randomNumber);
+        CarInfo carInfo = car.playOneRound(round, movingStatus);
+
+        assertThat(carInfo.name()).isEqualTo("kyeou");
+        assertThat(carInfo.currentPosition()).isEqualTo(newPosition);
+        assertThat(carInfo.currentRound()).isEqualTo(round);
     }
 
     @ParameterizedTest
-    @CsvSource(value = {"1, 1", "2, 2", "3, 3"})
-    @DisplayName("랜덤의 값이 4 이상이면 1만큼 더 움직인다.")
-    void playOneRound2(int position, int round) {
+    @CsvSource(value = {"true, 1", "true, 2", "true, 3"})
+    @DisplayName("RandomEngine 이 true 를 줄수록 position 의 값은 중첩돼서 올라간다.")
+    void playOneRound2(boolean movingStatus, int round) {
         Car car = new Car("aaa");
         for (int i = 0; i < round; i++) {
-            assertRandomNumberInRangeTest(() -> {
-                car.playOneRound(round);
-            }, MOVE);
-        }
-        assertRandomNumberInRangeTest(() -> {
-            CarInfo carInfo = car.playOneRound(round);
+            int position = i + 1;
+            CarInfo carInfo = car.playOneRound(i, movingStatus);
             assertThat(carInfo.currentPosition()).isEqualTo(position);
-        }, NOT_MOVE);
+        }
     }
 }
--- src/test/java/racingcar/service/domain/RandomEngineTest.java
+++ src/test/java/racingcar/service/domain/RandomEngineTest.java
@@ -1,4 +1,22 @@
-import static org.junit.jupiter.api.Assertions.*;
+package racingcar.service.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
 class RandomEngineTest {
-  
+    private final RandomEngine randomEngine = new RandomEngine();
+
+    @ParameterizedTest
+    @CsvSource(value = {"4, true", "0, false", "3, false", "9, true"})
+    @DisplayName("랜덤 값이 4 이상이면 true 를 반환하고, 4 이하면 false를 반환한다.")
+    void ifMove(int engineValue, boolean movingStatus) {
+        assertRandomNumberInRangeTest(() -> {
+            boolean result = randomEngine.ifMove();
+            assertThat(result).isEqualTo(movingStatus);
+        }, engineValue);
+    }
 }
